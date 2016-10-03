fb479e7833b598979834e6764cae0b3341aab38a
quallsma <quallsmg59@gmail.com>
2016-10-02 20:00:14 -0400

    Initial Commit
    Initial Commit

---
+++ NumeralConverter.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnitTestProject3
+{
+    public class NumeralConverter
+    {
+        private Dictionary<int, string> numerals;
+
+        public NumeralConverter()
+        {
+            numerals = new Dictionary<int, string>();
+            numerals.Add(1000, "M");
+            numerals.Add(900, "CM");
+            numerals.Add(500, "D");
+            numerals.Add(400, "CM");
+            numerals.Add(100, "C");
+            numerals.Add(90, "XC");
+            numerals.Add(50, "L");
+            numerals.Add(40, "XL");
+            numerals.Add(10, "X");
+            numerals.Add(9, "IX");
+            numerals.Add(5, "V");
+            numerals.Add(4, "IV");
+            numerals.Add(1, "I");
+        }
+
+        public string ArabicToRoman(int arabic)
+        {
+            StringBuilder roman = new StringBuilder();
+            foreach (var entry in numerals)
+            {
+                while (arabic >= entry.Key)
+                {
+                    roman.Append(entry.Value);
+                    arabic -= entry.Key;
+                }
+            }
+            return roman.ToString();
+        }
+
+        public int RomanTOArabic(string roman)
+        {
+            int arabic = 0;
+            int previousNum = 0;
+            int currentNum = 0;
+            for (int x = 0; x < roman.Length; x++)
+            {
+                string RomanCharacter = roman[x].ToString();
+                currentNum = numerals.FirstOrDefault(p => p.Value == RomanCharacter).Key;
+                if (currentNum > previousNum)
+                    arabic -= 2 * previousNum;
+                previousNum = currentNum;
+                arabic += currentNum;
+            }
+            return arabic;
+        }
+    }
+}
---
+++ NumeralConverterTest.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject3
+{
+    [TestClass]
+    public class NumeralConverterTest
+    {
+        [TestMethod]
+        public void ConvertToRoman1()
+        {
+            NumeralConverter converter = new NumeralConverter();
+            Assert.AreEqual("I", converter.ArabicToRoman(1));
+        }
+
+        [TestMethod]
+        public void ConvertToRoman3()
+        {
+            NumeralConverter converter = new NumeralConverter();
+            Assert.AreEqual("III", converter.ArabicToRoman(3));
+        }
+
+        [TestMethod]
+        public void ConvertToRoman9()
+        {
+            NumeralConverter converter = new NumeralConverter();
+            Assert.AreEqual("IX", converter.ArabicToRoman(9));
+        }
+
+        [TestMethod]
+        public void ConvertToRoman1066()
+        {
+            NumeralConverter converter = new NumeralConverter();
+            Assert.AreEqual("MLXVI", converter.ArabicToRoman(1066));
+        }
+
+        [TestMethod]
+        public void ConvertToRoman1989()
+        {
+            NumeralConverter converter = new NumeralConverter();
+            Assert.AreEqual("MCMLXXXIX", converter.ArabicToRoman(1989));
+        }
+
+        [TestMethod]
+        public void ConvertToArabic1()
+        {
+            NumeralConverter converter = new NumeralConverter();
+            Assert.AreEqual(1, converter.RomanTOArabic("I"));
+        }
+
+        [TestMethod]
+        public void ConvertToArabic3()
+        {
+            NumeralConverter converter = new NumeralConverter();
+            Assert.AreEqual(3, converter.RomanTOArabic("III"));
+        }
+
+        [TestMethod]
+        public void ConvertToArabic9()
+        {
+            NumeralConverter converter = new NumeralConverter();
+            Assert.AreEqual(9, converter.RomanTOArabic("IX"));
+        }
+
+        [TestMethod]
+        public void ConvertToArabic1066()
+        {
+            NumeralConverter converter = new NumeralConverter();
+            Assert.AreEqual(1066, converter.RomanTOArabic("MLXVI"));
+        }
+
+        [TestMethod]
+        public void ConvertToArabic1989()
+        {
+            NumeralConverter converter = new NumeralConverter();
+            Assert.AreEqual(1989, converter.RomanTOArabic("MCMLXXXIX"));
+        }
+    }
+}
